427d72571a46dbcacdaf3fa1d611bc42df1c30be
Maylor David Bustamante Mercado <maylorbustamante2001@gmail.com>
2022-10-14 17:59:50 +0200

    Falta aun mostrar las species

--- StarWars/src/app/diaglos/characther-info/characther-info.component.ts
+++ StarWars/src/app/diaglos/characther-info/characther-info.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { People } from 'src/app/interfaces/people-interface';
 import { DialogData } from 'src/app/interfaces/peopleInfo-interface';
+import { Specie } from 'src/app/interfaces/species-interface';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -11,11 +13,21 @@ import { environment } from 'src/environments/environment';
 export class CharactherInfoComponent implements OnInit {
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 
+  listSpecies: Specie[] = [];
   ngOnInit(): void {}
 
   //muestra la imagen por el id del characther
-  mostrarImagen(url : string) {
+  mostrarImagen(url: string) {
     let id = url.split('/')[5];
     return `${environment.apiUrlBaseImg}/characters/${id}.jpg`;
+  }
+
+  mostrarSpecie(people: People, specie: Specie) {
+    for (let i of people.species) {
+      if (i.toLowerCase() ==  specie.name) {
+        return specie.name;
+      }
+    }
+    return undefined;
   }
 }
