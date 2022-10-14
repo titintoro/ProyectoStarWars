860850e78bd92d835c88bf33440c529275286779
titintoro <titintola@gmail.com>
2022-10-14 10:16:27 +0200

    Arreglo errores

--- StarWars/src/app/app.module.ts
+++ StarWars/src/app/app.module.ts
@@ -8,33 +8,20 @@ import { AppComponent } from './app.component';
 import { MaterialsImportModule } from './materials-import/materials-import.module';
 import { PeopleListComponent } from './components/people-list/people-list.component';
 import { FilmListComponent } from './components/film-list/film-list.component';
-<<<<<<< HEAD
 import { SpeciesListComponent } from './components/species-list/species-list.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    PeopleListComponent,
-    FilmListComponent,
-    SpeciesListComponent,
-  ],
-=======
 import { PlanetsComponent } from './components/planets/planets.component';
 import { StarshipsComponent } from './components/starships/starships.component';
-
 @NgModule({
-<<<<<<< HEAD
+
   declarations: [
     AppComponent,
     PeopleListComponent,
     FilmListComponent,
     PlanetsComponent,
+    SpeciesListComponent,
     StarshipsComponent
   ],
-=======
-  declarations: [AppComponent, PeopleListComponent, FilmListComponent, PlanetsComponent],
->>>>>>> main
->>>>>>> main
+
   imports: [
     BrowserModule,
     HttpClientModule,
